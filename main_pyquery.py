5e16a5cba0f6d54883ede1b7ef88232c1e36b386
Jerry Lin <jerrylin3@quantatw.com>
2017-06-21 23:03:52 +0800

    取得文章內容(本文、時間、urls)

--- main_pyquery.py
+++ main_pyquery.py
@@ -1,63 +1,83 @@
 # -*- coding: UTF-8 -*-
-import requests
 import sys,os
 import re
-from bs4 import BeautifulSoup
 import urllib
 import time
 import calendar
 import sqlite3
 import datetime
 from pyquery import PyQuery as pq
+from random import randint
 main_url = 'https://www.ptt.cc'
 db_name = 'test.db'
 
 def startDownloadData(targetBoard,targetDate,targetFolder):
     targetUrl = 'https://www.ptt.cc/bbs/' + targetBoard + '/index.html'
-    dom = pq(url=targetUrl,cookies={'over18':'1'})
+    dom = pq(url=targetUrl,cookies={'over18' : '1'})
     contents = dom('.r-list-container').children('.r-list-sep').prev_all('.r-ent')
     for content in contents.items():
         webData = getWebData(content)
-        webDetailDataHtml = webData['contentHtml']
-        print(webDetailDataHtml)
-        webDetailData = getWebDetailData(webDetailDataHtml)
-        print(webDetailData)
+        if webData is None:
+            print('此文已被刪除,繼續爬下一篇')
+        else:
+            webDetailDataHtml = webData['html']
+            print('title : %s' % webData['title'])
+            print(webDetailDataHtml)
+            webDetailData = getWebDetailData(webDetailDataHtml)
+            print(webDetailData)
+            time_num = randint(1,3)
+            print('time_num : %d' % time_num)
+            time.sleep(time_num)
         
 
-#取得每則po文的內容
+#取得文章的內容與內容網址
 def getWebDetailData(webDetailDataHtml):
-    dom = pq(url=webDetailDataHtml)
-    contents = dom('.bbs-content')
-    contexts = contents
-    context = contexts('#main-content').remove('*').text()
-    return context
-#    for content in contents.items():
-#        if content('div').attr('class') == 'article-metaline':
-#            print(content('div').text())
-#        elif content('div').attr('class') == 'push':
-#            print(content('div').text())
-#        else:
-#        print(content('div').attr('class'))
-        
+    dom = pq(url=webDetailDataHtml,cookies={'over18' : '1'})
+
+    #取得po文時間
+    postDate = dom('.article-meta-value').eq(3).text()
+
+    #取得文章內的網址
+    webUrls = dom('#main-content').remove('.richcontent').find('a')
+    urls = []
+    for webUrl in webUrls.items():
+        urls.append(webUrl.attr('href'))
+    #將最後本文的網址去掉
+    urls.pop()
+
+    #取得文章的內文
+    article = dom('#main-content').remove('*').text()
+
+    webDetailDict = {
+                        'postDate'  :   postDate,
+                        'article'   :   article,
+                        'urls'      :   urls
+                    }
+
+    return webDetailDict
 
 #取得每則po文標題列的內容
 def getWebData(targetContent):
-    contentNrec = targetContent('.nrec').children().text()
-    contentDate = targetContent('.date').text()
-    contentHtml = main_url + targetContent('.title').children().attr('href')
-    contentTitle = targetContent('.title').children().text()
-    contentAuthor = targetContent('.author').text()
-    contentDict = {
-                          'contentNrec'   :   contentNrec,
-                          'contentDate'   :   contentDate,
-                          'contentHtml'   :   contentHtml,
-                          'contentTitle'  :   contentTitle,
-                          'contentAuthor' :   contentAuthor
-                        }
-    return contentDict
+    try:
+        nrec = targetContent('.nrec').children().text()
+        mmddDate = targetContent('.date').text()
+        html = main_url + targetContent('.title').children().attr('href')
+        title = targetContent('.title').children().text()
+        author = targetContent('.author').text()
+        webDict =   {
+                              'nrec'    :   nrec,
+                              'mmddDate':   mmddDate,
+                              'html'    :   html,
+                              'title'   :   title,
+                              'author'  :   author
+                    }
+    except:
+        return None
+    else:
+        return webDict
 
 if __name__ == "__main__":
-    targetBoard = 'stock'
+    targetBoard = 'Beauty'
     targetDate = '20170618'
     targetFolder = '~/Download/ptt_data/test_folder'
     startDownloadData(targetBoard,targetDate,targetFolder)
