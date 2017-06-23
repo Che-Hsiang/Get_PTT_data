a377e75974d8bbab9356b1828f65cf24283d75bf
Jerry Lin <jerrylin3@quantatw.com>
2017-06-24 01:16:52 +0800

    增加 新增資料到db的功能(1)

--- main_pyquery.py
+++ main_pyquery.py
@@ -6,29 +6,43 @@ import time
 import calendar
 import sqlite3
 import datetime
+import json
 from pyquery import PyQuery as pq
 from random import randint
-main_url = 'https://www.ptt.cc'
-db_name = 'test.db'
+mainUrl = 'https://www.ptt.cc'
+dbName = 'ptt.db'
 
 def startDownloadData(targetBoard,targetDate,targetFolder):
+    print('開始爬文章')
     targetUrl = 'https://www.ptt.cc/bbs/' + targetBoard + '/index.html'
+    targetUrlHead = 'https://www.ptt.cc/bbs/' + targetBoard + '/'
     dom = pq(url=targetUrl,cookies={'over18' : '1'})
     contents = dom('.r-list-container').children('.r-list-sep').prev_all('.r-ent')
+    pttDatas = []
     for content in contents.items():
+
         webData = getWebData(content)
         if webData is None:
             print('此文已被刪除,繼續爬下一篇')
         else:
-            webDetailDataHtml = webData['html']
-            print('title : %s' % webData['title'])
+            webDetailDataHtml = webData['articleUrl']
             print(webDetailDataHtml)
+            #文章代碼
+            articleCode = webDetailDataHtml.strip('.html').strip(targetUrlHead)
+            webData['articleCode'] = articleCode
+            webData['boardName'] = targetBoard
+
             webDetailData = getWebDetailData(webDetailDataHtml)
-            print(webDetailData)
-            time_num = randint(1,3)
-            print('time_num : %d' % time_num)
-            time.sleep(time_num)
-        
+
+            timeNum = randint(1,3)
+            time.sleep(timeNum)
+
+            pttData = {}
+            pttData = webData.copy()
+            pttData.update(webDetailData)
+            pttDatas.append(pttData)
+#    print(pttDatas)
+    insertDataToDb(pttDatas)
 
 #取得文章的內容與內容網址
 def getWebDetailData(webDetailDataHtml):
@@ -48,6 +62,7 @@ def getWebDetailData(webDetailDataHtml):
     #取得文章的內文
     article = dom('#main-content').remove('*').text()
 
+    #po文時間，文章內容，網址們
     webDetailDict = {
                         'postDate'  :   postDate,
                         'article'   :   article,
@@ -56,30 +71,79 @@ def getWebDetailData(webDetailDataHtml):
 
     return webDetailDict
 
-#取得每則po文標題列的內容
+#取得每頁文章標題列的內容
 def getWebData(targetContent):
     try:
         nrec = targetContent('.nrec').children().text()
         mmddDate = targetContent('.date').text()
-        html = main_url + targetContent('.title').children().attr('href')
+        html = mainUrl + targetContent('.title').children().attr('href')
         title = targetContent('.title').children().text()
         author = targetContent('.author').text()
+
+        #推數、月日時間、連結網址、標題、作者
         webDict =   {
                               'nrec'    :   nrec,
                               'mmddDate':   mmddDate,
-                              'html'    :   html,
+                              'articleUrl'    :   html,
                               'title'   :   title,
-                              'author'  :   author
+                              'authorId'  :   author
                     }
     except:
         return None
     else:
         return webDict
 
+def insertDataToDb(pttDatas):
+    print('準備新增資料')
+    checkDbExist()
+    try:
+        conn = sqlite3.connect(dbName)
+        cur = conn.cursor()
+        print('DB連線建立中')
+        for pttData in pttDatas:
+            print('爬尋資料中')
+            print(pttData)
+            sql =   """
+                        insert into articleData (boardName,articleCode,articleUrl,title,authorId,article,nrec)
+                        values('%(boardName)s','%(articleCode)s','%(articleUrl)s','%(title)s','%(authorId)s','%(article)s','%(nrec)s');
+                    """ % pttData
+            print(sql)
+            cur.execute(sql)
+        conn.commit()
+    except :
+        print('糟糕 sql 指令出了些問題')
+        conn.rollback()
+    finally:
+        conn.close()
+
+#確認db檔是否存在，若不存在則創db
+def checkDbExist():
+    if os.path.exists(dbName) is False:
+        print('DB不存在,即將建立DB')
+        createDb()
+    else:
+        print('DB存在')
+
+def createDb():
+    #print(create_table.sql)
+    f = open('ptt.db','w')
+    f.close()
+    s = open('create_table.json')
+    sql = json.load(s)
+    s.close()
+    conn = sqlite3.connect(dbName)
+    cur = conn.cursor()
+    cur.execute(sql['articleData'])
+    cur.execute(sql['pushData'])
+    conn.commit()
+    conn.close()
+    print('DB建立完畢')
+
 if __name__ == "__main__":
     targetBoard = 'Beauty'
     targetDate = '20170618'
     targetFolder = '~/Download/ptt_data/test_folder'
+#    checkDbExist()
     startDownloadData(targetBoard,targetDate,targetFolder)
 #    startDownloadImage()
 
